e2dc648c86f8868d75150fc9f57271e06165134f
Azedine Ouhadou <xperazazedine@gmail.com>
2022-06-22 14:18:51 +0100

    Customize the norm of dollar expander

--- includes/minishell.h
+++ includes/minishell.h
@@ -45,51 +45,57 @@ typedef struct command {
 	struct	command *next;
 } t_cmd;
 
-// typedef struct merging_tokens {
-// 	char    *merg;
-// 	struct	command *next;
-// } t_merge;
 
 //checker functions
-void	ft_check(char *command);
-int     command_checker(char *command);
+void				ft_check(char *command);
+int     			command_checker(char *command);
 
 //lists_utils_functions
-void    add_front(t_token** head, char *data);
-void	add_to_end(t_token** head, t_token *data);
-void    list_clear(t_token** head_ref);
-void	delete_node(t_token** head, t_token *del_node);
-int		lstsize(t_token *lst);
+void   				add_front(t_token** head, char *data);
+void				add_to_end(t_token** head, t_token *data);
+void    			list_clear(t_token** head_ref);
+void				delete_node(t_token** head, t_token *del_node);
+int					lstsize(t_token *lst);
+//cmds list_utls
+void    			add_cmd_to_end(t_cmd** command, char **new_data);
+t_cmd				*new_cmd_token(char **data);
 
-void    add_cmd_to_end(t_cmd** command, char **new_data);
-t_cmd	*new_cmd_token(char **data);
-
-t_token	*new_token(char *data, int size, t_token_type type);
+t_token				*new_token(char *data, int size, t_token_type type);
 static t_token_type	token_found(const char *cmd_line);
-int		get_word_token_size(char *cmd_line);
-void	get_next_token(char *cmd_line, int *size, t_token_type *type);
+int					get_word_token_size(char *cmd_line);
+void				get_next_token(char *cmd_line, int *size, t_token_type *type);
 
 //tokenizer_utils
-t_token	*ft_tokens(char *line);
-char	*ft_strncpy(char *dst, const char *src, int len);
-char	*ft_sub_str(const char *src, int len);
-void	remove_all_chars(char* str, char c);
-int		is_quotes(char *tok);
+t_token				*ft_tokens(char *line);
+char				*ft_strncpy(char *dst, const char *src, int len);
+char				*ft_sub_str(const char *src, int len);
+void				remove_all_chars(char* str, char c);
+int					is_quotes(char *tok);
 
 //Flaging Functions 
-void	flag_list(t_token *list);
-void	add_flag(t_token *tmp);
-int		check_cmd(char *tok);
-int		wich_redirection(char *red);
+void				flag_list(t_token *list);
+void				add_flag(t_token *tmp);
+int					check_cmd(char *tok);
+int					wich_redirection(char *red);
 
 //Syntax analyser
-int		invalid_token(char *node);
-int		is_operator(char *tok);
-int		syntax_validation(t_token *list);
+int					invalid_token(char *node);
+int					is_operator(char *tok);
+int					syntax_validation(t_token *list);
 
 //parser
-t_cmd	*creat_cmds(t_token *node);
-char	**remplir_tab(t_token *node, int start);
-int		count_size(t_token	*node, int start);
+t_cmd				*creat_cmds(t_token *node);
+char				**remplir_tab(t_token *node, int start);
+int					count_size(t_token	*node, int start);
+
+//epand dollar
+void				expand_dollar(t_cmd *node);
+
+//expander_utils
+char				*dollar_substr1(char *str);
+char				*dollar_substr(char *str);
+int					is_dollar(char *str);
+int					get_env_end(char *str);
+const char			*ft_strstr(const char *str, const char *to_find);
 
 #endif
---
+++ sources/expander_utils.c
@@ -0,0 +1,69 @@
+#include "../includes/minishell.h"
+
+const char	*ft_strstr(const char *str, const char *to_find)
+{
+	int	i;
+	int	len;
+
+	len = ft_strlen(to_find);
+	i = 0;
+	if (to_find[i++] == '\0')
+		return (str);
+	while (*str != '\0')
+	{
+		if (ft_strncmp(str, to_find, len) == 0)
+			return (str);
+		str++;
+	}
+	return (0);
+}
+
+int	is_dollar(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '$')
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+int	get_env_end(char *str)
+{
+	int	i;
+
+	i = 1;
+	while (str[i])
+	{
+		if (!ft_isalnum(str[i]) && str[i] != '_' && str[i] != '$')
+			return (i);
+		i++;
+	}
+	return(i);
+}
+
+char	*dollar_substr1(char *str)
+{
+	char	*sub;
+	char	*res;
+	int		end;
+	sub = strchr(str, '$');
+	end = get_env_end(sub);
+	res = ft_substr(sub, 1, end - 1);
+	return (res);
+}
+
+char	*dollar_substr(char *str)
+{
+	char	*sub;
+	char	*res;
+	int		end;
+	sub = strchr(str, '$');
+	end = get_env_end(sub);
+	res = ft_substr(sub, 0, end);
+	return (res);
+}
